841ce7319adea304da5b229241ea9ec0361f51a6
Andrew <andrew.montenigro@gmail.com>
2023-09-11 00:51:28 -0500

    wip: game cache manipulation scratch work

---
+++ src/gamedata.h
@@ -0,0 +1,11 @@
+#ifndef GAMEDATA_H
+#define GAMEDATA_H
+
+extern struct bin_header;
+extern struct bin_entry ;
+extern struct bin_file ;
+extern struct fpk_header ;
+extern struct fpk_entry ;
+extern struct fpk_file ;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,53 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
+
+struct BINheader {
+	char magic[4];    // fpk (unless it's a movie*uc.bin)
+	uint32_t entries; // number of files in archive
+};
+
+struct FPKheader {
+	uint32_t flag;     // null
+	uint32_t lzs_n;    // count of all files in archive
+	uint32_t lzs_p;    // size of archive header
+	uint32_t size;     // size of the whole file
+};
+
+struct FPKentry {
+	char name[20];     // entry name
+	uint32_t data_p;   // offset to entry
+	uint32_t dst_size; // compressed entry size
+	uint32_t src_size; // uncompressed entry size
+};
+
+struct FPKarchive {
+	struct FPKheader header;
+	struct FPKentry  entries[0];
+};
+
+int printBINheader(FILE *fpk) {
+	struct BINheader header;
+	fseek(fpk, 0, SEEK_SET);
+	fread(&header, sizeof(struct BINheader), 1, fpk);
+	return printf(
+		"magic: %s\nentries: %u\n",
+		header.magic, header.entries
+	);
+}
+
+int printFPKheader(FILE *fpk) {
+	struct FPKheader header;
+	fseek(fpk, 0, SEEK_SET);
+	fread(&header, sizeof(struct FPKheader), 1, fpk);
+	return printf(
+		"flag: %u\nlzs_n: %u\nlzs_p: %u\nsize: %u\n",
+		header.flag, header.lzs_n, header.lzs_p, header.size
+	);
+}
+
+//int listbinfiles(char[] binpath, char[] filelist /*out*/) {
+//	return -1;
+//}
 
 static void print_hello(GtkWidget *widget, gpointer data) {
 	g_print("Hello world\n");
@@ -22,7 +71,20 @@ static void activate(GtkApplication *app, gpointer user_data) {
 int main(int argc, char *argv[]) {
 	GtkApplication *app;
 	int status;
-	//app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS); // G_APPLICATION_DEFAULT_FLAGS is new so possibly system gtk4 is out of date
+	//const char path[] = "./disc/faceuc.bin";
+	const char path[] = "./disc/voiceuc.bin";
+	FILE *fpkarchive;
+
+	fpkarchive = fopen(&path, "rb");
+	if(fpkarchive != NULL) {
+		printBINheader(fpkarchive);
+		//printFPKheader(fpkarchive);
+		close(fpkarchive);
+	} else {
+		printf("couldn't open %s\n", path);
+	}
+	// G_APPLICATION_DEFAULT_FLAGS is new so possibly system gtk4 is out of date
+	//app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
 	app = gtk_application_new("com.github.aj-m.drakenbuild", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	status = g_application_run(G_APPLICATION(app), argc, argv);
